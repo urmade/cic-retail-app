f0c3b15c7f3556fd2cb4bd4815adbb6e71e53761
urmade <turban2009@live.de>
2022-09-13 21:14:46 +0200

    Reverted last debugging step

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ const port = process.env.PORT || 3000;
 if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
   config.baseURL = `http://localhost:${port}`;
 }
-else config.baseURL = process.env.URL;
 
 app.use(auth(config));
 
