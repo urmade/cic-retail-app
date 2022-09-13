cafdb93fc5c3cdbf7d3895c7bfdfaee31f788e25
urmade <turban2009@live.de>
2022-09-13 20:58:27 +0200

    init

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var router = require('express').Router();
+const { requiresAuth, auth } = require('express-openid-connect');
+
+router.get('/', function (req, res, next) {
+  let params = {
+    title: 'Auth0 Webapp sample Nodejs',
+    isAuthenticated: req.oidc.isAuthenticated()
+  };
+  if (params.isAuthenticated) 
+  {
+    params.firstName = req.oidc.idTokenClaims.given_name;
+  }
+  res.render('index', params);
+});
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page'
+  });
+});
+
+router.get("/loyaltyCard/:card",(req,res) => {
+  res.oidc.login({
+    authorizationParams: {
+      connection: req.params.card
+    },
+    returnTo: "/profile"
+  })
+})
+
+module.exports = router;
