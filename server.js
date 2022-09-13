5828b7a4ca416a2c19bde8846524084c7bb1a667
urmade <turban2009@live.de>
2022-09-13 21:10:42 +0200

    Debugging netlify

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const port = process.env.PORT || 3000;
 if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
   config.baseURL = `http://localhost:${port}`;
 }
+else config.baseURL = process.env.BASE_URL;
 
 app.use(auth(config));
 
